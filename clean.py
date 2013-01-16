0fe5c0285fbaf8ac81a99d04ecfb6e13eae91dfa
reggieag <reggieag@gmail.com>
2013-01-15 23:38:07 -0500

    Dropped more columns and consolidated code

--- clean.py
+++ clean.py
@@ -1,3 +1,6 @@
+#Kaggle: Titantic - Random Forest
+#This code cleans up the data for input into a simple Random Forest Algorithim 
+
 import csv
 import pandas as pd
 import numpy as np
@@ -6,10 +9,9 @@ cd D:\Kaggle\Titantic
 data = pd.read_csv('train.csv')
 
 #drop name column since cant really turn into floats
-data = data.drop('name', axis = 1)
+data = data.drop(['name','cabin','ticket'] axis = 1)
 #replace female with 1 and male with 0
-data['sex'] = data['sex'].replace('female',1)
-data['sex'] = data['sex'].replace('male',0)
+data['sex'] = data['sex'].replace(['male','female'],[0,1])
 #set missing ages to mean of ages
 data['age'] = data['age'].fillna(data['age'].mean())
 
