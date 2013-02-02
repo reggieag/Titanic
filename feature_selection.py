e7ae00b4eda58a3ca34346c4ac233729d3df717f
Reggie <Reggieag@gmail.com>
2013-02-02 20:49:35 +0000

    intial commit

--- feature_selection.py
+++ feature_selection.py
@@ -10,15 +10,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
+outputCSV = raw_input('What do you want to name the csv? ') + '.csv'
 
 def clean_data(Data_Frame):
 #drop name column since cant really turn into floats
-	Data_Frame = Data_Frame.drop(['name','cabin','ticket'], axis = 1)
+	Data_Frame = Data_Frame.drop(['name','cabin','ticket','embarked','parch'], axis = 1)
 #replace female with 1 and male with 0
 	Data_Frame['sex'] = Data_Frame['sex'].replace(['male','female'],[0,1])
-#replace embarked with integers and fill the null values
-	Data_Frame['embarked'] = Data_Frame['embarked'].replace(['S','C','Q'],[1,2,3])
-	Data_Frame['embarked'] = Data_Frame['embarked'].fillna(0)
 #set missing ages to mean of ages
 	Data_Frame['age'] = Data_Frame['age'].fillna(Data_Frame['age'].mean())
 	Data_Frame['fare'] = Data_Frame['fare'].fillna(Data_Frame['fare'].mean())
@@ -31,4 +29,4 @@ Forest = RandomForestClassifier(n_estimators = 100)
 Forest = Forest.fit(train_data.ix[:,"pclass":],train_data.ix[:,"survived"])
 
 Output = Forest.predict(test_data)
-np.savetxt("foo.csv",Output)
+np.savetxt(outputCSV,Output)
